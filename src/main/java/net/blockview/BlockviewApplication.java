b3eea439ccc09f0e6d14ba60b6d6d0c0fa002202
Chris Hatch <tofu@redlen.tl>
2016-05-15 10:57:19 +0700

    spring starter generated code change .gitignore to eclipse/maven

---
+++ src/main/java/net/blockview/BlockviewApplication.java
@@ -0,0 +1,12 @@
+package net.blockview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockviewApplication.class, args);
+	}
+}
